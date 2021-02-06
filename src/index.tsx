e608b3cdcc7a78be22c83958437bd7ec0ae020f0
Samoila Gabor <samoila.gabor@iquestgroup.com>
2021-02-06 08:54:30 +0200

    initial structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,21 @@
 import ReactDOM from 'react-dom';
+import { useState } from 'react';
 
 const App = () => {
-  return <h1>Hi</h1>;
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
+
+  const onClick = () => {
+    setCode(input);
+  }
+
+  return <div>
+    <textarea cols={100} rows={10} onChange={(e) => setInput(e.target.value)}></textarea>
+    <div>
+      <button onClick={onClick}>Submit</button>
+    </div>
+    <pre>{code}</pre>
+  </div>;
 }
 
 export default App;
