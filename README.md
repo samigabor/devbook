ddb02c59263db4c10dfdbf64c02949a2b2e4e814
Samoila Gabor <samoila.gabor@iquestgroup.com>
2021-02-07 10:51:37 +0200

    add unpkg plugin

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 `npx create-react-app jbook --template typescrypt`
 
 `npm i --save-exact esbuild-wasm@0.8.27`
+
+#### Get the link for react source code
+
+`npm view react dist.tarball`
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import ReactDOM from 'react-dom';
 import { useState, useEffect, useRef } from 'react';
 import * as esbuild from 'esbuild-wasm';
+import { unpkgPathPlugin } from './plugins/unpkg-path';
+import axios from 'axios';
 
 const App = () => {
   const [input, setInput] = useState('');
@@ -11,11 +13,17 @@ const App = () => {
     if (!ref.current) {
       return;
     }
-    const result = await ref.current.transform(input , {
-      loader: 'jsx',
-      target: 'es2015'
+    const result = await ref.current.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()],
+      define: {
+        'process.env.NODE_ENV': '"production"',
+        global: 'window'
+      }
     });
-    setCode(result.code);
+    setCode(result.outputFiles[0].text);
   }
 
   const startService = async () => {
